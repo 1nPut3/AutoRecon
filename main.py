5491e81140999c485fb5cde78d509a9dab50e9b8
JasonMcGee <jason0609@cox.net>
2017-11-20 12:12:50 -0700

    Added some exeptions

--- main.py
+++ main.py
@@ -2,28 +2,51 @@
 
 
 import subprocess, sys, time
+def menu_choices(scan_type):
+    print("")
+    if scan_type == "1":
+        print("[*] Starting Default Scan...")
+    elif scan_type == "2":
+        print("[*] Starting Light Scan...")
+    elif scan_type == "3":
+        print("[*] Starting Fast Scan...")
+    elif scan_type == "4":
+        print("[*] Starting Loud Scan...")
+    elif scan_type == "5":
+        print("[*] Starting Custom Scan...")
+    elif scan_type == "99":
+        print("[*] Exiting...")
+        exit()
+    else:
+        print("please enter a valid choice!")
 
 def menu():
-    subprocess.call(['clear',])
-    print("________________________Main Menu____________________________")
-    print("")
-    print("")
-    print("[1] Default Scan")
-    print("")
-    print("")
-    print("[2] Light Scan")
-    print("")
-    print("")
-    print("[3] Fast Scan")
-    print("")
-    print("")
-    print("[4] Loud Scan")
-    print("")
-    print("")
-    print("[5] Custom Scan")
-    print("")
-    print("")
-    scan_type = raw_input("What scan do you whant to preform?: ")
-    
-
+    try:
+        subprocess.call(['clear',])
+        print("________________________Main Menu____________________________")
+        print("")
+        print("")
+        print("[1]: Default Scan")
+        print("")
+        print("")
+        print("[2]: Light Scan")
+        print("")
+        print("")
+        print("[3]: Fast Scan")
+        print("")
+        print("")
+        print("[4]: Loud Scan")
+        print("")
+        print("")
+        print("[5]: Custom Scan")
+        print("")
+        print("")
+        print("[99]: Exit")
+        scan_type = raw_input("What scan do you want to run?: ")
+        menu_choices(scan_type)
+    except(KeyboardInterrupt):
+        print("")
+        print("[*] KeyboardInterrupt detected. ")
+        print("[*] Exiting...")
+        print("")
 menu()
