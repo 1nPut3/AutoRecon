43b0e2e61b2084c4d9b012698cbecf992a0c0b9d
JasonMcGee <jason0609@cox.net>
2017-11-25 11:24:39 -0700

    Added nikto scan and default_scan protocol

--- main.py
+++ main.py
@@ -3,28 +3,72 @@
 
 import subprocess, sys, time
 
+
+# starts nikto scan if website and not silent scan
+def nikto_scan(argument):
+    if argument == True:
+        print("[*] Starting nikto scan...")
+        subprocess.call(['nikto', '-h', target ,'-o', '/root/Documents/results.txt',])
+    elif argument == False:
+        print("[*] Detected not webpage...")
+        print("[*] Skipping nikto scan")
+    else:
+        # If error then abort task
+        print("[*] Error aborting...")
+
+def default_scan(target,website):
+    ###### Default scan protocol ######
+    # Start nmap scan
+    # If website start nikto scan
+    # If website and using word press start wpscan
+
+    print("[*] starting nmap scan(not done yet)...")
+    nikto_scan()
+    if website == 'y':
+        nikto_scan(True)
+    else:
+        nikto_scan(False)
+    print("[*] Starting wpscan...")
+    menu()
+
+
+# Ip address input function
 def ip():
     print("")
     address = raw_input("Type in the targets ip address: ")
     return(address)
 
+
+# If website function
+def is_website():
+    print("")
+    website = raw_input("Is this ip address a website?[y/n]: ")
+    return(website)
+
+# Pick from the menu choices fuction
 def menu_choices(scan_type):
     print("")
     if scan_type == "1":
         print("[*] Starting Default Scan...")
         target = ip()
+        website = is_website()
+        default_scan(target,website)
     elif scan_type == "2":
         print("[*] Starting Light Scan...")
         target = ip()
+        website = is_website()
     elif scan_type == "3":
         print("[*] Starting Fast Scan...")
         target = ip()
+        website = is_website()
     elif scan_type == "4":
         print("[*] Starting Loud Scan...")
         target = ip()
+        website = is_website()
     elif scan_type == "5":
         print("[*] Starting Custom Scan...")
         target = ip()
+        website = is_website()
     elif scan_type == "99":
         print("[*] Exiting...")
         exit()
@@ -55,8 +99,8 @@ def menu():
         print("[99]: Exit")
         scan_type = raw_input("What scan do you want to run?: ")
         menu_choices(scan_type)
+
     except(KeyboardInterrupt):
-        print("")
         print("[*] KeyboardInterrupt detected. ")
         print("[*] Exiting...")
         print("")
